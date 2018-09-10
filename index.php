28f5d3e1d81c6938cf434684eea42905e504c614
Kent Pirma <internet8@users.noreply.github.com>
2018-09-10 10:40:53 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Kent Pirma</title>
+</head>
+<body>
+	<h1>Kent Pirma</h1>
+	<p>See leht on valminud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames valminud veebilehed. Need ei oma mingit sügavat sisu ja nende kopeerimine ei oma mõtet.</p>
+	<p>Lisan veel ühe p tag-i järgmise tunni jaoks.</p>
+	<!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">-->
+	<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_3.jpg" alt="TLÜ Terra õppehoone">
+	<img src="../tlu_terra_600x400_2.jpg" alt="TLÜ Terra õppehoone">
+	<img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1" alt="TLÜ Terra õppehoone">
+	<p>Mul on sõber, kes ka teeb <a href="../../~kaspeel" target="_blank">veebi</a>.</p>
+	
+</body>
+</html>
