dba284396c8e0f987132299b44b5c6d19bde1f8a
Kent Pirma <internet8@users.noreply.github.com>
2018-09-24 11:25:11 +0300

    esimene leht andmebaasidega

---
+++ tund_4/foto2.php
@@ -0,0 +1,40 @@
+﻿<?php
+	//echo "Siin on minu esimene PHP!";
+	$name = "Kent";
+	$surname = "Pirma";
+	$dirToRead = "../../pics/";
+	$allFiles = array_slice(scandir($dirToRead), 2);
+	var_dump($allFiles);
+	
+	$picNum = mt_rand(0, count($allFiles)-1);
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		<?php
+			echo $name;
+			echo " ";
+			echo $surname;
+		?>
+	</title>
+</head>
+<body>
+	<h1>
+		<?php
+			echo $name ." " .$surname;
+		?>
+	</h1>
+	<p>See leht on valminud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames valminud veebilehed. Need ei oma mingit sügavat sisu ja nende kopeerimine ei oma mõtet.</p>
+	<p>Lisan veel ühe p tag-i järgmise tunni jaoks.</p>
+	<!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_3.jpg" alt="TLÜ Terra õppehoone">-->
+	
+	<?php
+		echo '<img src="' .$dirToRead .$allFiles[$picNum] .'" alt="pilt"><br>';
+	?>
+	
+</body>
+</html>
